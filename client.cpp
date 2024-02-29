b9329643c5b71fbba3e0c37025d72be27a755e51
Dominik Lastovcic <dominik.lastovcic@gmail.com>
2024-02-29 23:17:44 +0100

    Client connection can be closed by sending DISCONNECT by the client

--- client.cpp
+++ client.cpp
@@ -19,27 +19,27 @@
 #include <bits/stdc++.h>
 #include <sstream>
 
-
-#define PORT "3490" // the port client will be connecting to 
+#define PORT "3490" // the port client will be connecting to
 
 // colored print outputs
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define END     "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define END "\033[0m"
 
-#define MAXDATASIZE 100 // max number of bytes we can get at once 
+#define MAXDATASIZE 100 // max number of bytes we can get at once
 
 using namespace std;
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
 {
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
+    if (sa->sa_family == AF_INET)
+    {
+        return &(((struct sockaddr_in *)sa)->sin_addr);
     }
 
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+    return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
 
 void print_err(const char *msg)
@@ -57,7 +57,8 @@ void print_help(const char *msg)
     printf("%s%s%s", YELLOW, msg, END);
 }
 
-string waitForInput() {
+string waitForInput()
+{
     string input;
     cout << "Enter a command: ";
     getline(cin, input); // Wait for user input
@@ -66,7 +67,7 @@ string waitForInput() {
 
 int main(int argc, char *argv[])
 {
-    int sockfd, numbytes;  
+    int sockfd, numbytes;
     char buf[MAXDATASIZE];
     struct addrinfo hints, *servinfo, *p;
     int rv;
@@ -76,7 +77,7 @@ int main(int argc, char *argv[])
     const char *hostname;
     const char *port;
 
-    while(true)
+    while (true)
     {
         string userInput = waitForInput();
         stringstream ss(userInput);
@@ -84,25 +85,27 @@ int main(int argc, char *argv[])
         size_t found = userInput.find("CONNECT");
         if (found == 0)
         {
-         while (getline(ss, test, ' ')) {
- 
-        // store token string in the vector
-        stringVector.push_back(test);
-    }
-    // print the vector
-    // for (int i = 0; i < stringVector.size(); i++) {
-    //     cout << stringVector[i] << endl;
-    // }
+            while (getline(ss, test, ' '))
+            {
 
-        break;
+                // store token string in the vector
+                stringVector.push_back(test);
+            }
+            // print the vector
+            // for (int i = 0; i < stringVector.size(); i++) {
+            //     cout << stringVector[i] << endl;
+            // }
+
+            break;
         }
     }
-    
-    if (stringVector.size() != 3) {
+
+    if (stringVector.size() != 3)
+    {
         print_err("Error: ");
-        printf( "Invalid cmd arguments\n");
+        printf("Invalid cmd arguments\n");
         printf("Expected number of cmd arguments is 2. Received cmd argument number is %d\n", stringVector.size());
-        fprintf(stderr,"Usage: CONNECT <port> <hostname>. For example: CONNECT 3490 localhost \n%s",YELLOW, END);
+        fprintf(stderr, "Usage: CONNECT <port> <hostname>. For example: CONNECT 3490 localhost \n%s", YELLOW, END);
         exit(1);
     }
 
@@ -111,20 +114,24 @@ int main(int argc, char *argv[])
     hints.ai_socktype = SOCK_STREAM;
     hostname = stringVector[2].c_str();
     port = stringVector[1].c_str();
-    if ((rv = getaddrinfo(hostname, port, &hints, &servinfo)) != 0) {
+    if ((rv = getaddrinfo(hostname, port, &hints, &servinfo)) != 0)
+    {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
 
     // loop through all the results and connect to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
+    for (p = servinfo; p != NULL; p = p->ai_next)
+    {
         if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
+                             p->ai_protocol)) == -1)
+        {
             perror("client: socket");
             continue;
         }
 
-        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+        {
             close(sockfd);
             perror("client: connect");
             continue;
@@ -133,30 +140,39 @@ int main(int argc, char *argv[])
         break;
     }
 
-    if (p == NULL) {
+    if (p == NULL)
+    {
         fprintf(stderr, "client: failed to connect\n");
         return 2;
     }
 
     inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-            s, sizeof s);
+              s, sizeof s);
     printf("client: connecting to %s\n", s);
 
     freeaddrinfo(servinfo); // all done with this structure
-    while(true)
+    while (true)
     {
-    string userInput = waitForInput();
-     if (send(sockfd, &userInput[0], userInput.size(), 0) == -1)
-                perror("send");
+        string userInput = waitForInput();
+        if (send(sockfd, &userInput[0], userInput.size(), 0) == -1)
+            perror("send");
 
-    // if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-    //     perror("recv");
-    //     exit(1);
-    // }
+        if ((numbytes = recv(sockfd, buf, MAXDATASIZE - 1, 0)) == -1)
+        {
+            perror("recv");
+            exit(1);
+        }
 
-    // buf[numbytes] = '\0';
+        buf[numbytes] = '\0';
 
-    // printf("client: received '%s'\n",buf);
+        printf("client: received '%s'\n", buf);
+
+        char substr[] = "DISCONNECT";
+        char *result = strstr(buf, substr);
+        if (result != NULL)
+        {
+            break;
+        }
     }
     close(sockfd);
 
